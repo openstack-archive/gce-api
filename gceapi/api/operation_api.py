c8278b4b59cf3b6609b81878178cf5b853b9e647
Alexandre Levine <alevine@cloudscaling.com>
2014-02-01 16:59:00 +0400

    GCE-API support service for OpenStack
    Change-Id: Ie01ba537e37569d0d4e0e95a52b9b07fdc172986
    Implements: blueprint gce-api

---
+++ gceapi/api/operation_api.py
@@ -0,0 +1,167 @@
+#    Copyright 2013 Cloudscaling Group, Inc
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import uuid
+
+from gceapi.api import base_api
+from gceapi.api import scopes
+from gceapi import exception
+from gceapi.openstack.common import timeutils
+
+
+class API(base_api.API):
+    """GCE operation API"""
+
+    KIND = "operation"
+    PERSISTENT_ATTRIBUTES = ["id", "insert_time", "start_time", "end_time",
+                             "name", "type", "user", "status", "progress",
+                             "scope_type", "scope_name",
+                             "target_type", "target_name",
+                             "method_key", "item_id",
+                             "error_code", "error_message", "errors"]
+
+    def __init__(self, *args, **kwargs):
+        super(API, self).__init__(*args, **kwargs)
+        self._method_keys = {}
+        self._get_progress_methods = {}
+
+    def _get_type(self):
+        return self.KIND
+
+    def _get_persistent_attributes(self):
+        return self.PERSISTENT_ATTRIBUTES
+
+    def register_get_progress_method(self, method_key, method):
+        if method_key in self._get_progress_methods:
+            raise exception.Invalid()
+        # TODO(ft): check 'method' formal arguments
+        self._method_keys[method] = method_key
+        self._get_progress_methods[method_key] = method
+
+    def get_scopes(self, context, item):
+        return [scopes.construct(item["scope_type"], item["scope_name"])]
+
+    def get_item(self, context, name, scope=None):
+        operation = self._get_db_item_by_name(context, name)
+        if (operation is None or
+                operation["scope_type"] != scope.get_type() or
+                operation["scope_name"] != scope.get_name()):
+            raise exception.NotFound
+        operation = self._update_operation_progress(context, operation)
+        return operation
+
+    def get_items(self, context, scope=None):
+        operations = self._get_db_items(context)
+        if scope is not None:
+            operations = [operation for operation in operations
+                          if (operation["scope_type"] == scope.get_type() and
+                              operation["scope_name"] == scope.get_name())]
+        for operation in operations:
+            operation = self._update_operation_progress(context, operation)
+        return operations
+
+    def delete_item(self, context, name, scope=None):
+        # NOTE(ft): Google deletes operation with no check it's scope
+        item = self._get_db_item_by_name(context, name)
+        if item is None:
+            raise exception.NotFound
+        self._delete_db_item(context, item)
+
+    def _update_operation_progress(self, context, operation):
+        if operation["status"] == "DONE" or not operation.get("item_id"):
+            return operation
+        method_key = operation["method_key"]
+        get_progress = self._get_progress_methods[method_key]
+        operation_progress = get_progress(context, operation["item_id"])
+        if operation_progress is None:
+            return operation
+        operation.update(operation_progress)
+        if operation["progress"] == 100:
+            operation["status"] = "DONE"
+            operation["end_time"] = timeutils.isotime(None, True)
+        self._update_db_item(context, operation)
+        return operation
+
+    def construct_operation(self, context, op_type, target_type, target_name,
+                            scope):
+        operation_id = str(uuid.uuid4())
+        operation = {
+            "id": operation_id,
+            "name": "operation-" + operation_id,
+            "insert_time": timeutils.isotime(context.timestamp, True),
+            "user": context.user_name,
+            "type": op_type,
+            "target_type": target_type,
+            "target_name": target_name,
+            "scope_type": scope.get_type(),
+            "scope_name": scope.get_name(),
+        }
+        return operation
+
+    def save_operation(self, context, operation, start_time,
+                       get_progress_method, item_id, operation_result):
+        if isinstance(operation_result, Exception):
+            operation.update(_error_from_exception(operation_result))
+        operation["start_time"] = start_time
+        method_key = self._method_keys.get(get_progress_method)
+        if method_key is None or "error_code" in operation:
+            operation["progress"] = 100
+            operation["status"] = "DONE"
+            operation["end_time"] = timeutils.isotime(None, True)
+        else:
+            operation["progress"] = 0
+            operation["status"] = "RUNNING"
+            operation["method_key"] = method_key
+            if item_id is not None:
+                operation["item_id"] = item_id
+        return self._add_db_item(context, operation)
+
+    def update_operation(self, context, operation_id, operation_result):
+        operation = self._get_db_item_by_id(context, operation_id)
+        if operation is None:
+            # NOTE(ft): it may lead to hungup not finished operation in DB
+            return
+        if isinstance(operation_result, Exception):
+            operation.update(_error_from_exception(operation_result))
+        else:
+            operation.update(operation_result)
+        if operation["progress"] == 100 or "error_code" in operation:
+            operation["status"] = "DONE"
+            operation["end_time"] = timeutils.isotime(None, True)
+        operation.update(operation)
+        self._update_db_item(context, operation)
+
+
+def gef_final_progress(with_error=False):
+    progress = {"progress": 100}
+    if with_error:
+        progress["error_code"] = 500
+        progress["error_message"] = _('Internal server error')
+        progress["errors"] = [{
+           "code": "UNKNOWN_OS_ERROR",
+           "message": _("Operation finished with unknown error. "
+                        "See OpenStack logs.")
+        }]
+    return progress
+
+
+def is_final_progress(progress):
+    return progress is not None and (progress.get("progress") == 100 or
+                                     progress.get("error_code") is not None)
+
+
+def _error_from_exception(ex):
+    return {"errors": [{"code": ex.__class__.__name__, "message": str(ex)}],
+            "error_code": 500,
+            "error_message": _('Internal server error')}
---
+++ gceapi/config.py
@@ -0,0 +1,35 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# Copyright 2012 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo.config import cfg
+
+from gceapi.openstack.common.db.sqlalchemy import session as db_session
+from gceapi import paths
+from gceapi import version
+
+_DEFAULT_SQL_CONNECTION = 'sqlite:///' + paths.state_path_def('$sqlite_db')
+
+
+def parse_args(argv, default_config_files=None):
+    db_session.set_defaults(sql_connection=_DEFAULT_SQL_CONNECTION,
+                            sqlite_db='gceapi.sqlite')
+    cfg.CONF(argv[1:],
+             project='gceapi',
+             version=version.version_string(),
+             default_config_files=default_config_files)
