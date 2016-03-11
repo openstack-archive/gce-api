3e5cc8aa5f3283687b3b8195f68b692e0434a3cb
Swapnil Kulkarni (coolsvap) <me@coolsvap.net>
2016-03-11 13:19:54 +0530

    Replace deprecated LOG.warn with LOG.warning
    LOG.warn is deprecated. It still used in a few places.
    Updated to non-deprecated LOG.warning.
    
    Change-Id: I5220749e651a547c788722a3709124fe217e500c
    Closes-Bug:#1508442

--- gceapi/auth.py
+++ gceapi/auth.py
@@ -137,6 +137,6 @@ class GceapiKeystoneContext(wsgi.Middleware):
             # Fallback to deprecated role header:
             roles = req.headers.get('X_ROLE', '')
             if roles:
-                LOG.warn(_("Sourcing roles from deprecated X-Role HTTP "
+                LOG.warning(_("Sourcing roles from deprecated X-Role HTTP "
                            "header"))
         return [r.strip() for r in roles.split(',')]
