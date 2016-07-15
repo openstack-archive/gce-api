6b8589898ada77fa00ea4c9bb6714e3401dc1491
Ronald Bradford <ronald.bradford@gmail.com>
2016-07-15 12:22:08 -0400

    Replace oslo incubator eventlet use with oslo service
    Remove the legacy Oslo Incubator code for the eventlet_backdoor
    replacing with applicable oslo_service syntax.
    
    Change-Id: I8391422b6494d85f38e0aad4d87ff3bada3cb2d9

--- gceapi/service.py
+++ gceapi/service.py
@@ -26,10 +26,10 @@ import eventlet
 import greenlet
 from oslo_config import cfg
 from oslo_log import log as logging
+from oslo_service import eventlet_backdoor
 from oslo_utils import importutils
 
 from gceapi.i18n import _
-from gceapi.openstack.common import eventlet_backdoor
 from gceapi import wsgi
 
 LOG = logging.getLogger(__name__)
@@ -69,7 +69,7 @@ class Launcher(object):
 
         """
         self._services = []
-        self.backdoor_port = eventlet_backdoor.initialize_if_enabled()
+        self.backdoor_port = eventlet_backdoor.initialize_if_enabled(CONF)
 
     @staticmethod
     def run_server(server):
@@ -132,7 +132,6 @@ class ServiceLauncher(Launcher):
         for flag in CONF:
             flag_get = CONF.get(flag, None)
             # hide flag contents from log if contains a password
-            # should use secret flag when switch over to openstack-common
             if ("_password" in flag or "_key" in flag or
                     (flag == "sql_connection" and "mysql:" in flag_get)):
                 LOG.debug(_('%(flag)s : FLAG SET ') % {'flag': flag})
